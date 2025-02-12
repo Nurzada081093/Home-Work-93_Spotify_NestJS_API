0ec48a5ee847d2b2a2e3662c80fbdb74f5e31c85
Zhumakadyrova Nurzada <zhumakadyrova.nurzat081093@gmail.com>
2025-02-12 23:08:09 +0600

    В файлах: main.ts прописала путь для изображений, app.module.ts прописала модели. В схемах: album.schema.ts и track.schema.ts прописала типы. Далее реализовала получение всех исполнителей, получение одного исполнителя, создание исполнителя и удаление исполнителя.

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -1,4 +1,55 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Artist, ArtistDocument } from '../schemas/artist.schema';
+import { Model } from 'mongoose';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CreateArtistDto } from './create-artist.dto';
 
 @Controller('artists')
-export class ArtistsController {}
+export class ArtistsController {
+  constructor(
+    @InjectModel(Artist.name)
+    private artistModel: Model<ArtistDocument>,
+  ) {}
+  @Get()
+  getAll() {
+    return this.artistModel.find();
+  }
+  @Get(':id')
+  getOne(@Param('id') id: string) {
+    return this.artistModel.findById({ _id: id });
+  }
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('image', { dest: './public/images/artists' }),
+  )
+  async create(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() artistDto: CreateArtistDto,
+  ) {
+    const newArtist = new this.artistModel({
+      name: artistDto.name,
+      description: artistDto.description,
+      image: file && file.filename ? '/images/artists/' + file.filename : null,
+    });
+    return await newArtist.save();
+  }
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    const artist = await this.artistModel.findByIdAndDelete(id);
+    if (!artist) {
+      throw new NotFoundException('Artist not found');
+    }
+    return { message: 'Artist has been deleted successfully' };
+  }
+}
